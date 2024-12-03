91a25917347b0a720dc8a3c418f0085007aa0a34
Hetti Arachchilage Harshana Madhuwantha Silva <151857717+hahmsilva99@users.noreply.github.com>
2024-12-03 10:56:23 +0530

    remove unnessecary files

--- react/front-end-phase-3/front-end-phase-3/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Message {
-    text: string;
-    isUser: boolean;
-  }
-  
-  export interface Candidate {
-    id: number;
-    title: string;
-  }
--- react/front-end-phase-3/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',  // Make sure this includes your React components
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
